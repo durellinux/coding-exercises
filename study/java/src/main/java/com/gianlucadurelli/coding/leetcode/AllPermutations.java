326cf0e13afea9a81d2c763601bc9e4cfb983ea5
Gianluca Durelli <gianluca.durelli@bottega52.it>
2021-06-26 14:20:22 +0200

    Changed data structure.

--- study/java/src/main/java/com/gianlucadurelli/coding/leetcode/AllPermutations.java
+++ study/java/src/main/java/com/gianlucadurelli/coding/leetcode/AllPermutations.java
@@ -5,7 +5,7 @@ import java.util.*;
 //https://leetcode.com/explore/interview/card/facebook/53/recursion-3/292
 public class AllPermutations {
 	public List<List<Integer>> permute(int[] nums) {
-		Deque<List<Integer>> solution = new LinkedList<>();
+		Queue<List<Integer>> solution = new LinkedList<>();
 
 		if (Objects.isNull(nums) || nums.length == 0) {
 			return new ArrayList<>();
@@ -18,7 +18,7 @@ public class AllPermutations {
 		for(int i = 1; i < nums.length; i++) {
 			int currentSolutions = solution.size();
 			for(int s = 0; s < currentSolutions; s ++) {
-				List<Integer> prevSol = solution.pop();
+				List<Integer> prevSol = solution.remove();
 				for(int p = 0; p <= prevSol.size(); p++) {
 					List<Integer> newSol = new ArrayList<>(prevSol);
 					newSol.add(p, nums[i]);
