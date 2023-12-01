1d9c01b413283b37c54f8a5ea690ef5ecb3455aa
Gianluca Durelli <gianluca.durelli@beekeeper.io>
2023-12-01 23:51:13 +0100

    Advent of code - day 1

--- study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/Trebuchet.java
+++ study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/Trebuchet.java
@@ -1,30 +1,73 @@
 package com.gianlucadurelli.coding.adventofcode.year2023;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 // https://adventofcode.com/2023/day/1
 public class Trebuchet {
-    public int solve(List<String> input) {
+    private static final Map<String, Integer> stringValueMap = Map.of(
+        "one", 1,
+        "two", 2,
+        "three", 3,
+        "four", 4,
+        "five", 5,
+        "six", 6,
+        "seven", 7,
+        "eight", 8,
+        "nine", 9
+    );
+
+    private static final Map<String, Integer> digitValueMap = Map.of(
+        "0", 0,
+        "1", 1,
+        "2", 2,
+        "3", 3,
+        "4", 4,
+        "5", 5,
+        "6", 6,
+        "7", 7,
+        "8", 8,
+        "9", 9
+    );
+
+    public int solve1(List<String> input) {
         int sum = 0;
 
         for (String value: input) {
-            sum += parseString(value);
+            sum += parseString(value, digitValueMap);
         }
 
         return sum;
     }
 
-    private int parseString(String value) {
+    public int solve2(List<String> input) {
+        Map<String, Integer> fullMap = new HashMap<>();
+        fullMap.putAll(stringValueMap);
+        fullMap.putAll(digitValueMap);
+
+        int sum = 0;
+
+        for (String value: input) {
+            sum += parseString(value, fullMap);
+        }
+
+        return sum;
+    }
+
+    private int parseString(String value, Map<String, Integer> lookupTable) {
         Integer firstDigit = null;
         Integer secondDigit = null;
 
-        for (char c: value.toCharArray()) {
-            if (c >= '0' && c <='9') {
+        for (int i = 0; i < value.length(); i++) {
+            Optional<Integer> matchvalue = matchString(value, i, lookupTable);
+            if (matchvalue.isPresent()) {
                 if (firstDigit == null) {
-                    firstDigit = c - '0';
+                    firstDigit = matchvalue.get();
                 }
 
-                secondDigit = c - '0';
+                secondDigit = matchvalue.get();
             }
         }
 
@@ -33,5 +76,15 @@ public class Trebuchet {
         }
 
         return firstDigit * 10 + secondDigit;
+    }
+
+    private Optional<Integer> matchString(String input, int index, Map<String, Integer> lookupTable) {
+        for (String value: lookupTable.keySet()) {
+            if (input.regionMatches(index, value, 0, value.length())) {
+                return Optional.of(lookupTable.get(value));
+            }
+        }
+
+        return Optional.empty();
     }
 }
--- study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/TrebuchetTest.java
+++ study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/TrebuchetTest.java
@@ -14,13 +14,27 @@ public class TrebuchetTest extends TestCase {
     @Test
     public void test1() {
         Assertions.assertThat(
-            solver.solve(List.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"))
+            solver.solve1(List.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"))
         ).isEqualTo(142);
     }
 
     @Test
-    public void testSolution() {
-        System.out.println(solver.solve(AdventOfCode2023Inputs.TREBUCHET_INPUT));
+    public void test2() {
+        Assertions.assertThat(
+            solver.solve2(List.of("two1nine" ,"eightwothree" ,"abcone2threexyz" ,"xtwone3four" ,"4nineeightseven2" ,"zoneight234" ,"7pqrstsixteen"))
+        ).isEqualTo(281);
+    }
+
+    @Test
+    public void testSolution1() {
+        Assertions.assertThat(solver.solve1(AdventOfCode2023Inputs.TREBUCHET_INPUT))
+            .isEqualTo(54390);
+    }
+
+    @Test
+    public void testSolution2() {
+        Assertions.assertThat(solver.solve2(AdventOfCode2023Inputs.TREBUCHET_INPUT))
+            .isEqualTo(54277);
     }
 
 }
