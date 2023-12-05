e20dfa891505df9cf1e9ab920ce5bbe450b86778
Gianluca Durelli <durellinux@gmail.com>
2023-12-05 19:19:24 +0100

    Advent of Code - Day 5 (Part 2)

--- study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/IfYouGiveASeedAFertilizer.java
+++ study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/IfYouGiveASeedAFertilizer.java
@@ -1,21 +1,14 @@
 package com.gianlucadurelli.coding.adventofcode.year2023;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class IfYouGiveASeedAFertilizer {
-
     private record ProblemEntry(long source, long destination, long length) {}
     private record Interval(long start, long end) {}
-    private record Point(long value, boolean isInput) {}
+    private record MapInterval(long start, long end, long destinationStart) {}
 
-    public long solve1(
-        String seedsInput,
+    public long solve(
+        List<Interval> seeds,
         List<String> seedsToSoilsInput,
         List<String> soilToFertilizerInput,
         List<String> fertilizerToWaterInput,
@@ -24,7 +17,6 @@ public class IfYouGiveASeedAFertilizer {
         List<String> temperatureToHumidityInput,
         List<String> humidityToLocationInput
     ) {
-        List<Interval> seeds = parseSeeds1(seedsInput);
         Map<Long, ProblemEntry> seedsToSoils = parseInput(seedsToSoilsInput);
         Map<Long, ProblemEntry> soilToFertilizer = parseInput(soilToFertilizerInput);
         Map<Long, ProblemEntry> fertilizerToWater = parseInput(fertilizerToWaterInput);
@@ -39,61 +31,33 @@ public class IfYouGiveASeedAFertilizer {
         List<Interval> lights = doMap(waters, waterToLight);
         List<Interval> temperatures = doMap(lights, lightToTemperature);
         List<Interval> humidities = doMap(temperatures, temperatureToHumidity);
-        List<Long> locations = doMap(humidities, humidityToLocation);
+        List<Interval> locations = doMap(humidities, humidityToLocation);
 
-        return locations.stream().reduce(Long::min).orElse(0L);
+        return locations.stream()
+                .map(v -> v.start)
+                .reduce(Long::min).orElse(0L);
     }
 
-    public long solve2(
-        String seedsInput,
-        List<String> seedsToSoilsInput,
-        List<String> soilToFertilizerInput,
-        List<String> fertilizerToWaterInput,
-        List<String> waterToLightInput,
-        List<String> lightToTemperatureInput,
-        List<String> temperatureToHumidityInput,
-        List<String> humidityToLocationInput
-    ) {
-        List<Interval> seeds = parseSeeds2(seedsInput);
-        Map<Long, ProblemEntry> seedsToSoils = parseInput(seedsToSoilsInput);
-        Map<Long, ProblemEntry> soilToFertilizer = parseInput(soilToFertilizerInput);
-        Map<Long, ProblemEntry> fertilizerToWater = parseInput(fertilizerToWaterInput);
-        Map<Long, ProblemEntry> waterToLight = parseInput(waterToLightInput);
-        Map<Long, ProblemEntry> lightToTemperature = parseInput(lightToTemperatureInput);
-        Map<Long, ProblemEntry> temperatureToHumidity = parseInput(temperatureToHumidityInput);
-        Map<Long, ProblemEntry> humidityToLocation = parseInput(humidityToLocationInput);
-
-        List<Interval> soils = doMap(seeds, seedsToSoils);
-        List<Interval> fertilizers = doMap(soils, soilToFertilizer);
-        List<Interval> waters = doMap(fertilizers, fertilizerToWater);
-        List<Interval> lights = doMap(waters, waterToLight);
-        List<Interval> temperatures = doMap(lights, lightToTemperature);
-        List<Interval> humidities = doMap(temperatures, temperatureToHumidity);
-        List<Long> locations = doMap(humidities, humidityToLocation);
-
-        return locations.stream().reduce(Long::min).orElse(0L);
-    }
-
-    private List<Interval> parseSeeds1(String input) {
+    public List<Interval> parseSeeds1(String input) {
         List<Interval> result = new ArrayList<>();
         String[] values = input.split(" ");
 
-        for (int i = 0; i < values.length; i++) {
-            long start = Long.valueOf(values[i].strip(), 10);
+        for (String value : values) {
+            long start = Long.valueOf(value.strip(), 10);
             result.add(new Interval(start, start));
         }
 
         return result;
     }
 
-    private List<Interval> parseSeeds2(String input) {
+    public List<Interval> parseSeeds2(String input) {
         List<Interval> result = new ArrayList<>();
         String[] values = input.split(" ");
 
         for (int i = 0; i < values.length; i+=2) {
             long start = Long.valueOf(values[i].strip(), 10);
-            long end = Long.valueOf(values[i+1].strip(), 10);
-            result.add(new Interval(start, end));
+            long length = Long.valueOf(values[i+1].strip(), 10);
+            result.add(new Interval(start, start + length - 1));
         }
 
         return result;
@@ -112,107 +76,61 @@ public class IfYouGiveASeedAFertilizer {
         return thisMap;
     }
 
-    private List<Long> doMap(List<Interval> sources, Map<Long, ProblemEntry> nextMap) {
-//        List<Long> orderedSources = nextMap.keySet().stream().sorted().toList();
+    private List<Interval> doMap(List<Interval> sources, Map<Long, ProblemEntry> nextMap) {
         List<Interval> nextValues = new ArrayList<>();
-        sources.sort((i1, i2) -> Math.toIntExact(i1.start - i2.start));
+        sources.sort(Comparator.comparingLong(i -> i.start));
 
-        Map<Long, Interval> intervalMap = new HashMap<>();
-        for (Interval i: sources) {
-            intervalMap.put(i.start, i);
+        List<MapInterval> tmpIntervals = new ArrayList<>(nextMap.values().stream().map(v -> new MapInterval(v.source, v.source + v.length - 1, v.destination)).toList());
+        tmpIntervals.sort(Comparator.comparingLong(i -> i.start));
+
+
+        List<MapInterval> allMapIntervals = new ArrayList<>();
+        allMapIntervals.add(new MapInterval(Long.MIN_VALUE, tmpIntervals.get(0).start - 1, Long.MIN_VALUE));
+        for (MapInterval tmpInterval : tmpIntervals) {
+            if (allMapIntervals.get(allMapIntervals.size() - 1).end < tmpInterval.start - 1) {
+                allMapIntervals.add(new MapInterval(
+                        allMapIntervals.get(allMapIntervals.size() - 1).end + 1,
+                        tmpInterval.start - 1,
+                        allMapIntervals.get(allMapIntervals.size() - 1).end + 1
+                ));
+            }
+            allMapIntervals.add(tmpInterval);
         }
+        allMapIntervals.add(new MapInterval(tmpIntervals.get(tmpIntervals.size() - 1).end + 1, Long.MAX_VALUE, tmpIntervals.get(tmpIntervals.size() - 1).end + 1));
 
-        List<Long> nextMapPoints = new ArrayList<>();
-        nextMapPoints.addAll(nextMap.keySet());
-        nextMapPoints.sort(Long::compareTo);
-
-        Iterator<Long> nextMapValueIterator = nextMapPoints.iterator();
         Iterator<Interval> intervalIterator = sources.iterator();
+        Iterator<MapInterval> nextMapValueIterator = allMapIntervals.iterator();
 
-        Long lastMapValue = Long.MIN_VALUE;
-        Long currentMapValue = nextMapValueIterator.next();
-
+        MapInterval mapValue = nextMapValueIterator.next();
         while(intervalIterator.hasNext()) {
             Interval interval = intervalIterator.next();
 
             while(interval != null) {
-                interval = generateIntervals(interval, lastMapValue, currentMapValue, nextMap, nextValues);
 
-                if (interval != null) {
-                    lastMapValue = currentMapValue;
-                    currentMapValue = nextMapValueIterator.hasNext() ? nextMapValueIterator.next() : Long.MAX_VALUE;
+                while(interval.start > mapValue.end) {
+                    mapValue = nextMapValueIterator.next();
                 }
-            }
-        }
 
-        for (Point p: points) {
-            if (p.isInput == false) {
-                lastMapValue = p.value;
-            } else {
-                if (lastMapValue == null) {
-                    Interval i = intervalMap.get(p.value);
-                }
-            }
-        }
-
-        long prev = 0;
-        long next = orderedSources.get(0);
-        if (sources.get(0).start < orderedSources.get(0)) {
-
-        }
-
-        for (Interval value: sources) {
-            Integer minValPosition = null;
-            for (int index = 0; index < orderedSources.size(); index++) {
-                if (orderedSources.get(index) < value) {
-                    minValPosition = index;
-                }
-            }
-
-            if (minValPosition == null) {
-                nextValues.add(value);
-            } else {
-                Long nextSource = orderedSources.get(minValPosition);
-                long nextLength = nextMap.get(nextSource).length;
-                long nextDestination = nextMap.get(nextSource).destination;
-
-                if (value - nextSource > nextLength - 1) {
-                    nextValues.add(value);
-                } else {
-                    nextValues.add(nextDestination + value - nextSource);
-                }
+                interval = generateIntervals(interval, mapValue, nextValues);
             }
         }
 
         return nextValues;
     }
 
-    Interval generateIntervals(Interval interval, long prev, long next, Map<Long, ProblemEntry> nextMap, List<Interval> result) {
-        if (interval.end < prev) {
-
-        } else if (interval.start < prev && prev <= interval.end && interval.end < next ) {
-
-        } else if (interval.start == prev && interval.end == next) {
-
-        } else if (prev < interval.start && interval.start < next && next < interval.end) {
-
-        } else if (interval.start == next) {
-
-        } else if (prev <= interval.start && interval.end < next) {
-
+    Interval generateIntervals(Interval interval, MapInterval mapInterval, List<Interval> result) {
+        if (interval.start >= mapInterval.start && interval.end <= mapInterval.end) {
+            long startDestination = mapInterval.destinationStart + (interval.start - mapInterval.start);
+            long endDestination = startDestination + (interval.end - interval.start);
+            result.add(new Interval(startDestination, endDestination));
+            return null;
+        } else if (interval.start <= mapInterval.end && interval.end > mapInterval.end) {
+            long startDestination = mapInterval.destinationStart + (interval.start - mapInterval.start);
+            long endDestination = startDestination + (mapInterval.end - interval.start);
+            result.add(new Interval(startDestination, endDestination));
+            return new Interval(mapInterval.end + 1, interval.end);
         } else {
-            throw new UnsupportedOperationException();
-        }
-
-        if (prev != null && intervalEnd <= prev) {
-            long nextLength = nextMap.get(prev).length;
-            long nextDestination = nextMap.get(prev).destination;
-
-            if (value - nextSource > nextLength - 1) {
-                nextValues.add(value);
-            } else {
-                nextValues.add(nextDestination + value - nextSource);
-            }
+            throw new UnsupportedOperationException(interval + " - " + mapInterval);
         }
     }
 }
--- study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/IfYouGiveASeedAFertilizerTest.java
+++ study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/IfYouGiveASeedAFertilizerTest.java
@@ -24,8 +24,8 @@ public class IfYouGiveASeedAFertilizerTest extends TestCase {
 
     @Test
     public void test1() {
-        Assertions.assertThat(solver.solve1(
-            "79 14 55 13",
+        Assertions.assertThat(solver.solve(
+                solver.parseSeeds1("79 14 55 13"),
             List.of("50 98 2", "52 50 48"),
             List.of("0 15 37", "37 52 2", "39 0 15"),
             List.of("49 53 8", "0 11 42", "42 0 7", "57 7 4"),
@@ -36,11 +36,10 @@ public class IfYouGiveASeedAFertilizerTest extends TestCase {
         )).isEqualTo(35);
     }
 
-
     @Test
     public void testSolution1() {
-        Assertions.assertThat(solver.solve1(
-            SEEDS_INPUT_SEEDS,
+        Assertions.assertThat(solver.solve(
+            solver.parseSeeds1(SEEDS_INPUT_SEEDS),
             SEEDS_INPUT_SOILS,
             SEEDS_INPUT_FERTILIZERS,
             SEEDS_INPUT_WATERS,
@@ -51,4 +50,31 @@ public class IfYouGiveASeedAFertilizerTest extends TestCase {
         )).isEqualTo(403695602);
     }
 
+    @Test
+    public void test2() {
+        Assertions.assertThat(solver.solve(
+                solver.parseSeeds2("79 14 55 13"),
+                List.of("50 98 2", "52 50 48"),
+                List.of("0 15 37", "37 52 2", "39 0 15"),
+                List.of("49 53 8", "0 11 42", "42 0 7", "57 7 4"),
+                List.of("88 18 7", "18 25 70"),
+                List.of("45 77 23", "81 45 19", "68 64 13"),
+                List.of("0 69 1", "1 0 69"),
+                List.of("60 56 37", "56 93 4")
+        )).isEqualTo(46);
+    }
+
+    @Test
+    public void testSolution2() {
+        Assertions.assertThat(solver.solve(
+                solver.parseSeeds2(SEEDS_INPUT_SEEDS),
+                SEEDS_INPUT_SOILS,
+                SEEDS_INPUT_FERTILIZERS,
+                SEEDS_INPUT_WATERS,
+                SEEDS_INPUT_LIGHTS,
+                SEEDS_INPUT_TEMPERATURES,
+                SEEDS_INPUT_HUMIDITIES,
+                SEEDS_INPUT_LOCATIONS
+        )).isEqualTo(219529182);
+    }
 }
