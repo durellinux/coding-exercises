ca510534b2d90498d0adc2c5398b8ef531cd201e
Gianluca Durelli <durellinux@gmail.com>
2023-12-23 12:05:01 +0100

    Giving up on Day 21... again

--- study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/Day21StepCounter.java
+++ study/java/src/main/java/com/gianlucadurelli/coding/adventofcode/year2023/Day21StepCounter.java
@@ -2,112 +2,206 @@ package com.gianlucadurelli.coding.adventofcode.year2023;
 
 import java.util.*;
 import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Day21StepCounter {
 
-    public record Point(int r, int c) {}
+    private record Point(int x, int y) {}
+
+    private record Fraction(int numerator, int denominator, boolean positive) {
+        public static Fraction from(Point p) {
+            // TODO: Fix
+            return new Fraction(p.y, p.x, true);
+        }
+    }
+
+    private record Segment(Point p1, Point p2) {
+        public boolean belongsToSegment(Point p, int R, int C) {
+            int xMin = Math.min(p1.x, p2.x);
+            int xMax = Math.max(p1.x, p2.x);
+
+            int yMin = Math.min(p1.y, p2.y);
+            int yMax = Math.max(p1.y, p2.y);
+
+//            boolean xInRange = xMin <= (p.x + kr * R) && (p.x + kr * R) <= xMax;
+//            boolean yInRange = xMin <= (p.x + kc * C) && (p.x + kc * C) <= xMax;
+//            boolean sameSlope = (p2.x - px + kr * R) && (p.y *)
+
+            return false;
+        }
+    }
+
     public record Step(Point p, int step) {}
+
+    public record WalkStatsWithFrontiers(List<Long> frontiers, Set<Step> selectedSteps, WalkStats stats) {
+        public long visitedPlotsAtStep(int step) {
+            if (step > frontiers.size()) {
+                return -1;
+            }
+
+            long result = 0;
+            for (int i = step; i >= 0; i -= 2) {
+                result += frontiers.get(i);
+            }
+
+            return result;
+        }
+
+        public Step minLeft(int C) {
+            List<Step> steps = selectedSteps.stream().filter(s -> s.p.y == 0).sorted(Comparator.comparingInt(a -> a.step)).map(s -> new Step(new Point(s.p.x, C - 1), s.step + 1)).toList();
+            return steps.get(0);
+        }
+
+        public Step minRight(int C) {
+            List<Step> steps = selectedSteps.stream().filter(s -> s.p.y == C - 1).sorted(Comparator.comparingInt(a -> a.step)).map(s -> new Step(new Point(s.p.x, 0), s.step + 1)).toList();
+            return steps.get(0);
+        }
+
+        public Step minUp(int R) {
+            List<Step> steps = selectedSteps.stream().filter(s -> s.p.y == 0).sorted(Comparator.comparingInt(a -> a.step)).map(s -> new Step(new Point(R - 1, s.p.y), s.step  +1)).toList();
+            return steps.get(0);
+        }
+
+        public Step minDown(int R) {
+            List<Step> steps = selectedSteps.stream().filter(s -> s.p.y == R -1).sorted(Comparator.comparingInt(a -> a.step)).map(s -> new Step(new Point(0, s.p.y), s.step + 1)).toList();
+            return steps.get(0);
+        }
+    }
+
+    public record WalkStats(long even, long odd, long minStep, long maxStep) {}
 
     public long solve(List<String> input, int maxSteps) {
         char[][] garden = parseInput(input);
         Point start = getInitialPoint(garden);
-        garden[start.r][start.c] = '.';
-        return makeSteps(garden, start, maxSteps, this::nextPlots);
-//        long solution = 0;
-//        for (int s = maxSteps; s >= 0; s-=2) {
-//            solution += frontiers.get(s);
-//        }
-//
-//        return solution;
+        garden[start.x][start.y] = '.';
+        WalkStatsWithFrontiers stats = makeSteps(garden, start, maxSteps, this::nextPlots, s -> s.step == maxSteps);
+        System.out.println(stats);
+        return stats.visitedPlotsAtStep(maxSteps);
     }
 
     public long solve2(List<String> input, int maxSteps) {
         char[][] garden = parseInput(input);
         Point start = getInitialPoint(garden);
-        garden[start.r][start.c] = '.';
-        return makeSteps(garden, start, maxSteps, this::nextPlotsWithOverFlow);
-//        long solution = 0;
-//        for (int s = maxSteps; s >= 0; s-=2) {
-//            solution += frontiers.get(s);
-//        }
-//
-//        return solution;
+        garden[start.x][start.y] = '.';
+
+        int R = garden.length;
+        int C = garden[0].length;
+
+        WalkStatsWithFrontiers stats = makeSteps(garden, start, 400, this::nextPlots, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+
+        System.out.println("Garden: " + R + " x " + C);
+        System.out.println("Start: " + start.x + " x " + start.y);
+        System.out.println("Frontiers size: " + stats.frontiers.size());
+        System.out.println(stats.stats);
+
+        if (maxSteps <= stats.stats.minStep) {
+            return stats.visitedPlotsAtStep(maxSteps);
+        }
+
+        long fullGardens = maxSteps / stats.stats.maxStep;
+
+        Step firstUp = stats.minUp(R);
+        Step firstDown = stats.minDown(R);
+        Step firstLeft = stats.minLeft(C);
+        Step firstRight = stats.minRight(C);
+
+
+        WalkStatsWithFrontiers leftStats = makeSteps(garden, firstLeft.p, 400, this::nextPlots, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+        WalkStatsWithFrontiers rightStats = makeSteps(garden, firstRight.p, 400, this::nextPlots, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+        WalkStatsWithFrontiers upStats = makeSteps(garden, firstUp.p, 400, this::nextPlots, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+        WalkStatsWithFrontiers downStats = makeSteps(garden, firstDown.p, 400, this::nextPlots, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+
+        System.out.println(leftStats.stats);
+        System.out.println(rightStats.stats);
+        System.out.println(upStats.stats);
+        System.out.println(leftStats.stats);
+
+        return stats.stats().even()
+                + leftStats.visitedPlotsAtStep(maxSteps - firstLeft.step)
+                + rightStats.visitedPlotsAtStep(maxSteps - firstRight.step)
+                + upStats.visitedPlotsAtStep(maxSteps - firstUp.step)
+                + downStats.visitedPlotsAtStep(maxSteps - firstDown.step);
     }
 
-    private long makeSteps(char[][] garden, Point start, int maxSteps, BiFunction<Point, char[][], List<Point>> computeNextPlots) {
-//        List<Long> frontiers = new ArrayList<>();
-//        for (int s = 0; s <= maxSteps; s++) {
-//            frontiers.add(0L);
-//        }
 
-        long even = 1;
-        long odd = 0;
+    public List<Long> plot2(List<String> input, int maxSteps) {
+        char[][] garden = parseInput(input);
+        Point start = getInitialPoint(garden);
+        garden[start.x][start.y] = '.';
 
-//        Set<Point> visitedPlots = new HashSet<>();
+        int R = garden.length;
+        int C = garden[0].length;
+
+        WalkStatsWithFrontiers stats = makeSteps(garden, start, maxSteps, this::nextPlotsWithOverFlow, s -> s.p.x == R - 1 || s.p.y == C - 1 || s.p.x == 0 || s.p.y == 0);
+
+//        System.out.println(stats.stats);
+        return stats.frontiers;
+    }
+
+    private WalkStatsWithFrontiers makeSteps(char[][] garden, Point start, int maxSteps, BiFunction<Point, char[][], List<Point>> computeNextPlots, Predicate<Step> predicate) {
+        Set<Step> visitedPlots = new HashSet<>();
+        List<Long> frontiers = new ArrayList<>();
+
+        Set<Step> allSteps = new HashSet<>();
+        allSteps.add(new Step(start, 0));
 
         Queue<Step> toVisit = new LinkedList<>();
-//        Map<Point, Integer> visited = new HashMap<>();
-        Map<Integer, Set<Point>> visited = new HashMap<>();
-
-//        Set<Point> visited = new HashSet<>();
+        Set<Point> visited = new HashSet<>();
 
         toVisit.add(new Step(start, 0));
-//        visited.put(start, 0);
-        visited.put(-1, Collections.EMPTY_SET);
-        visited.put(0, Set.of(start));
-        visited.put(-2, new HashSet<>());
-        visited.get(-2).add(start);
-//        frontiers.set(0, 1L);
+        visited.add(start);
+        frontiers.add(1L);
 
-        int curStep = 0;
         while (!toVisit.isEmpty()) {
             Step current = toVisit.poll();
 
-            if (current.step == maxSteps) {
-//                visitedPlots.add(current.p);
-                continue;
+            if (current.step > maxSteps) {
+                break;
             }
 
-            if (current.step != curStep) {
-                if (current.step % 500 == 0) {
-                    System.out.println("Analyzing step: " + current.step);
-                }
-                visited.remove(current.step - 2);
-                curStep = current.step;
+            if (predicate.test(current)) {
+                visitedPlots.add(current);
             }
 
-            int currentStep = current.step;
             List<Point> nextPositions = computeNextPlots.apply(current.p, garden);
             for (Point p: nextPositions) {
-                if (!visited.get(currentStep - 1).contains(p) && !visited.getOrDefault(currentStep + 1, Collections.EMPTY_SET).contains(p)) {
+                if (!visited.contains(p)) {
                     Step next = new Step(p, current.step + 1);
-//                    frontiers.set(current.step + 1, frontiers.get(current.step + 1) + 1);
-                    if ((currentStep + 1) % 2 == 0) {
-                        even++;
-                    } else {
-                        odd++;
-                    }
-                    Set<Point> nextSet = visited.getOrDefault(currentStep + 1, new HashSet<>());
-                    nextSet.add(p);
-                    visited.put(currentStep + 1, nextSet);
+                    visited.add(p);
                     toVisit.add(next);
-//                    System.out.println("New Point: " + p + " - " + visited.size());
-                } else {
-//                    System.out.println("Analyzing: " + current + " - skipping " + p + " - seen: " + visited.get(p) + " - " + (current.step));
+
+                    if (frontiers.size() < current.step + 2) {
+                        frontiers.add(0L);
+                    }
+
+                    long old = frontiers.get(current.step + 1);
+                    frontiers.set(current.step + 1, old + 1);
+
+                    if (maxSteps % 2 == (current.step + 1) % 2) {
+                        visitedPlots.add(next);
+                    }
+
+                    allSteps.add(next);
                 }
             }
         }
 
-        if (maxSteps % 2 == 0) {
-            return even;
+        printMatrix(garden, visitedPlots);
+
+        long even = 0;
+        long odd = 0;
+        for (int f = 0; f < frontiers.size(); f++) {
+            if ( f % 2 == 0) {
+                even += frontiers.get(f);
+            } else {
+                odd += frontiers.get(f);
+            }
         }
 
-        return odd;
 
-//        System.out.println(visited);
-//        System.out.println(frontiers);
-//        return frontiers;
+        int minStep = visitedPlots.stream().map(s -> s.step).min(Comparator.comparingInt(a -> a)).orElse(0);
+        int maxStep = visitedPlots.stream().map(s -> s.step).max(Comparator.comparingInt(a -> a)).orElse(0);
+        return new WalkStatsWithFrontiers(frontiers, visitedPlots, new WalkStats(even, odd, minStep, maxStep));
     }
 
     private List<Point> nextPlots(Point start, char[][] garden) {
@@ -119,8 +213,8 @@ public class Day21StepCounter {
         for (int d = 0; d < 4; d++) {
             int dr = drs[d];
             int dc = dcs[d];
-            int r = start.r + dr;
-            int c = start.c + dc;
+            int r = start.x + dr;
+            int c = start.y + dc;
             Optional<Character> value = getValue(r, c, garden);
             if (value.isPresent() && value.get().equals('.')) {
                 points.add(new Point(r, c));
@@ -141,10 +235,10 @@ public class Day21StepCounter {
         for (int d = 0; d < 4; d++) {
             int dr = drs[d];
             int dc = dcs[d];
-            int r = (start.r + dr);
-            int c = (start.c + dc);
+            int r = (start.x + dr);
+            int c = (start.y + dc);
             Point pointInRange = pointInRange(r, c, R, C);
-            Optional<Character> value = getValue(pointInRange.r, pointInRange.c, garden);
+            Optional<Character> value = getValue(pointInRange.x, pointInRange.y, garden);
             if (value.isPresent() && value.get().equals('.')) {
                 points.add(new Point(r, c));
             }
@@ -201,5 +295,25 @@ public class Day21StepCounter {
         }
 
         return matrix;
+    }
+
+    void printMatrix(char[][] garden, Set<Step> steps) {
+        Map<Point, Integer> stepForPoint = new HashMap<>();
+        steps.forEach(s -> stepForPoint.put(s.p, s.step));
+
+        int R = garden.length;
+        int C = garden[0].length;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                Point p = new Point(r, c);
+                if (stepForPoint.containsKey(p)) {
+                    System.out.print("O");
+                } else {
+                    System.out.print(garden[r][c]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/Day21StepCounterTest.java
+++ study/java/src/test/java/com/gianlucadurelli/coding/adventofcode/year2023/Day21StepCounterTest.java
@@ -4,28 +4,19 @@ import com.gianlucadurelli.coding.adventofcode.AdventOfCodeTestHelpers;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day21StepCounterTest {
 
     private static final Day21StepCounter solver = new Day21StepCounter();
     private static final List<String> TEST_INPUT = AdventOfCodeTestHelpers.loadTestInput(Day21StepCounter.class);
+    private static final List<String> TEST_INPUT_2 = AdventOfCodeTestHelpers.loadWithSuffix(Day21StepCounter.class, "-test2.txt");
     private static final List<String> INPUT = AdventOfCodeTestHelpers.loadInput(Day21StepCounter.class);
 
     @Test
     public void test1() {
         Assertions.assertThat(solver.solve(TEST_INPUT, 6)).isEqualTo(16);
-    }
-
-    @Test
-    public void test1_enum() {
-        long last = 0;
-//        for (int i = 0; i < 1000; i++) {
-//            long newValue = solver.solve2(TEST_INPUT, i);
-//            System.out.println("f("+i+") = " + newValue + " - delta: " + (newValue - last) );
-//            last = newValue;
-//        }
-        Assertions.assertThat(solver.solve2(TEST_INPUT, 26501365)).isEqualTo(16);
     }
 
     @Test
@@ -35,22 +26,86 @@ public class Day21StepCounterTest {
 
     @Test
     public void test2() {
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 6)).isEqualTo(16);
-//        Assertions.assertThat(solver.solve2(INPUT, 6)).isEqualTo(37);
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 10)).isEqualTo(50);
-//        Assertions.assertThat(solver.solve2(INPUT, 10)).isEqualTo(89);
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 50)).isEqualTo(1594);
-//        Assertions.assertThat(solver.solve2(INPUT, 50)).isEqualTo(2130);
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 100)).isEqualTo(6536);
-//        Assertions.assertThat(solver.solve2(INPUT, 100)).isEqualTo(8624);
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 500)).isEqualTo(167004);
-//        Assertions.assertThat(solver.solve2(TEST_INPUT, 1000)).isEqualTo(668697);
-        Assertions.assertThat(solver.solve2(TEST_INPUT, 5000)).isEqualTo(16733044);
+        Assertions.assertThat(solver.solve2(TEST_INPUT, 6)).isEqualTo(16);
+        Assertions.assertThat(solver.solve2(TEST_INPUT, 10)).isEqualTo(50);
     }
 
+    @Test
+    public void test_2() {
+        Assertions.assertThat(solver.solve(TEST_INPUT_2, 50)).isEqualTo(16);
+    }
+
+    @Test
+    public void test2_plot() {
+        for (int i = 499; i < 500; i++) {
+            List<Long> solution = solver.plot2(TEST_INPUT, i);
+            long area = area(i);
+            List<Long> expected =frontiers(solution.size());
+            List<Long> diff = error(solution, expected);
+//            System.out.println(i + " " + solver.plot2(TEST_INPUT, i) + " - " + area + " - " + (solution - area));
+            System.out.println(solution);
+            System.out.println(expected);
+            System.out.println(diff);
+//            for (int tmp = 0; tmp < solution.size(); tmp++) {
+//                System.out.println(tmp + " " + diff.get(tmp));
+//            }
+        }
+    }
+
+    long area(long s) {
+        long i = s / 2;
+        if ( s % 2 == 0) {
+            return 1 + 8 * i * (i+1) / 2;
+        }
+
+        return 4 + 4*i + 8*i*(i+1)/2;
+    }
+
+    List<Long> error(List<Long> actual, List<Long> expected) {
+        List<Long> diff = new ArrayList<>();
+        for (int i = 0; i < actual.size(); i++) {
+            diff.add(expected.get(i) -actual.get(i));
+        }
+
+        return diff;
+    }
+
+    List<Long> frontiers(long s) {
+        List<Long> f = new ArrayList<>();
+        for (int i = 0; i < s; i++) {
+            f.add(perimeter(i));
+        }
+
+        return f;
+    }
+
+    long perimeter(long s) {
+        if (s == 0) {
+            return 1;
+        }
+
+        return 4 * (s);
+    }
+
+    private long polyFit(int step) {
+        double x = step * 1.0;
+        return  Math.round(8.1773566158360870e+002 * Math.pow(x,0)
+                + -7.5791319316010515e+001 * Math.pow(x,1)
+                +  3.4332597056310563e+000 * Math.pow(x,2)
+                + -5.7228780202924302e-002 * Math.pow(x,3)
+                +  7.4030909312120614e-004 * Math.pow(x,4)
+                + -6.2825241083701417e-006 * Math.pow(x,5)
+                +  3.5666791122072450e-008 * Math.pow(x,6)
+                + -1.3488749574404663e-010 * Math.pow(x,7)
+                +  3.2820364404242919e-013 * Math.pow(x,8)
+                + -4.6533614827148485e-016 * Math.pow(x,9)
+                +  2.6567965734824193e-019 * Math.pow(x,10)
+                +  1.4077662924135407e-022 * Math.pow(x,11)
+                + -1.9850790107829447e-025 * Math.pow(x,12));
+    }
 
     @Test
     public void testSolution2() {
-        Assertions.assertThat(solver.solve2(INPUT, 64)).isEqualTo(3562);
+        Assertions.assertThat(solver.solve2(INPUT, 500)).isEqualTo(3562);
     }
 }
