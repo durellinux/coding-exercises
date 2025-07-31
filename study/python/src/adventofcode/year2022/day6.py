bd0a5f961bd40622611a0fc8994670add66eec60
Gianluca Durelli <durellinux@gmail.com>
2025-07-31 22:25:18 +0200

    AoC 2022: Day 6 - fancy

--- study/python/src/adventofcode/year2022/day6.py
+++ study/python/src/adventofcode/year2022/day6.py
@@ -1,5 +1,27 @@
 from collections import deque
 
+class UniqueQueue:
+    dequeue: deque[str]
+    char_set: set[str]
+
+    def __init__(self):
+        self.dequeue = deque()
+        self.char_set = set()
+
+    def enqueue(self, char: str):
+        if char in self.char_set:
+            val = self.dequeue.popleft()
+            self.char_set.remove(val)
+            while val != char:
+                val = self.dequeue.popleft()
+                self.char_set.remove(val)
+
+        self.dequeue.append(char)
+        self.char_set.add(char)
+
+    def length(self) -> int:
+        return len(self.dequeue)
+
 def solve_day6_part1(input_txt):
     return solve_day6(input_txt, 4)
 
@@ -8,23 +30,14 @@ def solve_day6_part2(input_txt):
 
 def solve_day6(data: str, target_length: int) -> int:
     characters = list(data)
-    dequeue = deque()
-    char_set = set()
+    unique_queue = UniqueQueue()
 
     index = 0
     for char in characters:
         index += 1
-        if char in char_set:
-            val = dequeue.popleft()
-            char_set.remove(val)
-            while val != char:
-                val = dequeue.popleft()
-                char_set.remove(val)
+        unique_queue.enqueue(char)
 
-        dequeue.append(char)
-        char_set.add(char)
-
-        if len(dequeue) == target_length:
+        if unique_queue.length() == target_length:
             break
 
     return index
