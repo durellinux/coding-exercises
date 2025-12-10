c8c348b84fce10afd116253e143ae5af620ba14e
Gianluca Durelli <durellinux@gmail.com>
2025-12-10 06:11:27 +0100

    AoC 2025: Day 9 (Part 2)

--- study/csharp/src/gianlucadurelli/adventofcode/year2025/Day9.cs
+++ study/csharp/src/gianlucadurelli/adventofcode/year2025/Day9.cs
@@ -23,15 +23,180 @@ public class Day9
         return maxArea;
     }
 
+    /*
+     * The whole area can be decomposed in smaller rectangles (created by the intersection of all the lines in the figure, drawn to infinity).
+     * These rectangles are all inside or all outside of the polygon.
+     * Hence, for each rectangle, we need to check only one of its point to determine whether the rectangle is in or out.
+     *
+     * Loops over all the pair of coordinates, if all the inner rectangles are valid, then check the area and update the max.
+     */
     public long Part2(IList<string> input)
     {
         IList<Segment<Coordinate>> segments = ParseInput2(input);
-        IList<Segment<Coordinate>> rowSegments = segments.Where(s => s.Start.Row == s.End.Row).ToList();
-        IList<Segment<Coordinate>> colSegments = segments.Where(s => s.Start.Col == s.End.Col).ToList();
-        IList<int> rowBreakPoints = rowSegments.SelectMany(s => new List<int> { s.Start.Row, s.End.Row }).ToList();
-        IList<int> colBreakpoints = colSegments.SelectMany(s => new List<int> { s.Start.Col, s.End.Col }).ToList();
+        IList<Segment<Coordinate>> rowSegments = segments
+            .Where(s => s.Start.Row == s.End.Row)
+            .OrderBy(s => s.Start.Row)
+            .Select(s => s.Start.Col < s.End.Col ? s : new Segment<Coordinate>(s.End, s.Start))
+            .ToList();
+        IList<Segment<Coordinate>> colSegments = segments
+            .Where(s => s.Start.Col == s.End.Col)
+            .OrderBy(s => s.Start.Col)
+            .Select(s => s.Start.Row < s.End.Row ? s : new Segment<Coordinate>(s.End, s.Start))
+            .ToList();
+        List<int> rowBreakPoints = segments
+            .SelectMany(s => new List<int> { s.Start.Row, s.End.Row })
+            .ToHashSet()
+            .OrderBy(v => v)
+            .ToList();
+        List<int> colBreakpoints = segments
+            .SelectMany(s => new List<int> { s.Start.Col, s.End.Col })
+            .ToHashSet()
+            .OrderBy(v => v)
+            .ToList();
+        IList<Coordinate> coordinates = segments
+            .SelectMany(s => new List<Coordinate> { s.Start, s.End })
+            .ToHashSet()
+            .ToList();
 
-        return 0;
+        // Cache for fast lookup to check when an inner rectangle is inside the polygon
+        var rectangleCache = new Dictionary<(Coordinate, Coordinate, Coordinate), bool>();
+        
+        long maxArea = 0;
+        for (int c1Id = 0; c1Id < coordinates.Count - 1; c1Id++) {
+            for (int c2Id = c1Id + 1; c2Id < coordinates.Count; c2Id++)
+            {
+                Coordinate c1 = coordinates[c1Id];
+                Coordinate c2 = coordinates[c2Id];
+
+                // Check if all the inner rectangles between the two coordinates are inside the polygon
+                var isValid = true;
+                foreach (var rectangle in GetRectangles(c1, c2, rowBreakPoints, colBreakpoints))
+                {
+                    var rectangleValid = rectangleCache.ContainsKey(rectangle)
+                        ? rectangleCache[rectangle]
+                        : IsInside(rectangle.Item3, rowSegments, colSegments);
+                    rectangleCache[rectangle] = rectangleValid;
+                    isValid = isValid && rectangleValid;
+                    if (!isValid)
+                    {
+                        break;
+                    }
+                }
+
+                if (!isValid)
+                {
+                    continue;
+                }
+
+                long currentArea = (Math.Abs(c1.Row - c2.Row) + 1) * (long) (Math.Abs(c1.Col - c2.Col) + 1);
+                if (currentArea > maxArea)
+                {
+                    maxArea = currentArea;
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+
+    /* Check if a point is inside the polygon.
+     * A point is inside if:
+     * - is on one of the segments
+     * - drawing a horizontal line from the point to the 0 vertical axis, this line crosses an odd number of segments
+     *   it might cross vertexes, if it crosses one vertex, it will then cross another, hence the vertexes should be counted only 1
+     */
+    private bool IsInside(Coordinate coordinate, IList<Segment<Coordinate>> rowSegments,
+        IList<Segment<Coordinate>> colSegments)
+    {
+        if (rowSegments
+            .Any(s => s.Start.Row == coordinate.Row && s.Start.Col <= coordinate.Col && s.End.Col >= coordinate.Col))
+        {
+            return true;
+        }
+
+        if (colSegments
+            .Any(s => s.Start.Col == coordinate.Col && s.Start.Row <= coordinate.Row && s.End.Row >= coordinate.Row))
+        {
+            return true;
+        }
+
+        int segmentsCrossed = 0;
+        int vertexCrossed = 0;
+
+        foreach (var colSegment in colSegments)
+        {
+            if (colSegment.Start.Col >= coordinate.Col)
+            {
+                break;
+            }
+
+            if (colSegment.Start == coordinate || colSegment.End == coordinate)
+            {
+                vertexCrossed++;
+            } else if (colSegment.Start.Row <= coordinate.Row && colSegment.End.Row >= coordinate.Row)
+            {
+                segmentsCrossed++;
+            }
+        }
+        
+        return (segmentsCrossed + vertexCrossed / 2) % 2 == 1;
+    }
+
+    /*
+     * Build all the inner rectangles between c1 and c2.
+     * The rectangle is all inside or all outside of the polygon.
+     * For each one of them selects a representative coordinate that needs to be checked.
+     */
+    private IEnumerable<(Coordinate, Coordinate, Coordinate)> GetRectangles(Coordinate c1, Coordinate c2, List<int> rowBreakPoints, List<int> colBreakpoints)
+    {
+        int colStartPos = colBreakpoints.BinarySearch(c1.Col);
+        int colEndPos = colBreakpoints.BinarySearch(c2.Col);
+        int rowStartPos = rowBreakPoints.BinarySearch(c1.Row);
+        int rowEndPos = rowBreakPoints.BinarySearch(c2.Row);
+
+        if (colEndPos < colStartPos)
+        {
+            (colStartPos, colEndPos) = (colEndPos, colStartPos);
+        }
+
+        if (rowEndPos < rowStartPos)
+        {
+            (rowStartPos, rowEndPos) = (rowEndPos, rowStartPos);
+        }
+
+
+        if (colStartPos != colEndPos && rowStartPos != rowEndPos)
+        {
+            for (int rPos1 = rowStartPos; rPos1 <= rowEndPos - 1; rPos1++)
+            {
+                for (int cPos1 = colStartPos; cPos1 <= colEndPos - 1; cPos1++)
+                {
+                    Coordinate start = new Coordinate(rowBreakPoints[rPos1], colBreakpoints[cPos1]);
+                    Coordinate end = new Coordinate(rowBreakPoints[rPos1 + 1], colBreakpoints[cPos1 + 1]);
+                    Coordinate representative = new Coordinate(rowBreakPoints[rPos1] + 1, colBreakpoints[cPos1] + 1);
+                    yield return (start, end, representative);
+                }
+            }
+        } else if (colStartPos == colEndPos)
+        {
+            for (int rPos1 = rowStartPos; rPos1 <= rowEndPos - 1; rPos1++)
+            {
+                Coordinate start = new Coordinate(rowBreakPoints[rPos1], colBreakpoints[colStartPos]);
+                Coordinate end = new Coordinate(rowBreakPoints[rPos1 + 1], colBreakpoints[colStartPos]);
+                Coordinate representative = new Coordinate(rowBreakPoints[rPos1] + 1, colStartPos);
+                yield return (start, end, representative);
+            }
+        }
+        else if (rowStartPos == rowEndPos)
+        {
+            for (int cPos1 = colStartPos; cPos1 <= colEndPos - 1; cPos1++)
+            {
+                Coordinate start = new Coordinate(rowBreakPoints[rowStartPos], colBreakpoints[cPos1]);
+                Coordinate end = new Coordinate(rowBreakPoints[rowStartPos], colBreakpoints[cPos1 + 1]);
+                Coordinate representative = new Coordinate(rowStartPos, colBreakpoints[cPos1] + 1);
+                yield return (start, end, representative);
+            }
+        }
     }
 
     private IList<Coordinate> ParseInput1(IList<string> input)
@@ -49,9 +214,9 @@ public class Day9
     private IList<Segment<Coordinate>> ParseInput2(IList<string> input)
     {
         IList<Segment<Coordinate>> segments = new List<Segment<Coordinate>>();
-        Coordinate? Start = null;
-        Coordinate End;
+        Coordinate? start = null;
 
+        input.Add(input[0]);
         for (int lineId = 0; lineId < input.Count; lineId++)
         {
             string line = input[lineId];
@@ -60,19 +225,18 @@ public class Day9
 
             if (lineId == 0)
             {
-                Start = coordinate;
+                start = coordinate;
             }
-            else if(Start != null)
+            else if(start != null)
             {
-                End = coordinate;
-
-                if (Start.Col != End.Col && Start.Row != End.Row)
+                var end = coordinate;
+                if (start.Col != end.Col && start.Row != end.Row)
                 {
                     throw new ArgumentException("Values are not in order!");
                 }
 
-                segments.Add(new Segment<Coordinate>(Start, End));
-                Start = End;
+                segments.Add(new Segment<Coordinate>(start, end));
+                start = end;
             }
         }
 
--- study/csharp/tests/gianlucadurelli/adventofcode/year2025/Day9Test.cs
+++ study/csharp/tests/gianlucadurelli/adventofcode/year2025/Day9Test.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
+using src.gianlucadurelli.adventofcode.utils;
 using src.gianlucadurelli.adventofcode.year2025;
 using tests.gianlucadurelli.adventofcode.utils;
 
@@ -24,5 +25,22 @@ public class Day9Test
         IList<string> content = FileUtils.ReadAocInputLines(2025, "day9");
         long result = solver.Part1(content);
         result.Should().Be(4749838800);
+    }
+    
+    [Test]
+    public void Part2_Example()
+    {
+        IList<string> content = FileUtils.ReadAocInputLines(2025, "day9-test");
+        long result = solver.Part2(content);
+        result.Should().Be(24);
+    }
+    
+    [Test]
+    [Ignore("Takes ~30s to run")]
+    public void Part2()
+    {
+        IList<string> content = FileUtils.ReadAocInputLines(2025, "day9");
+        long result = solver.Part2(content);
+        result.Should().Be(1624057680);
     }
 }
